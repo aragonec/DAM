1ec317ce987dbb1b805fb4e49c72d9adacffb3e1
aragonec <aragon1@gmail.com>
2021-06-25 22:52:12 -0600

    Tabs-Kotlin

--- Covid/app/src/main/java/com/example/covid/MainActivity.kt
+++ Covid/app/src/main/java/com/example/covid/MainActivity.kt
@@ -11,37 +11,12 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var txtUsuario: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txtUsuario=findViewById(R.id.usuario)
-        var txtPassword:EditText = findViewById(R.id.pass)
-        Log.e("Prueba", txtUsuario.text.toString())
-        print(txtPassword.text)
-        login(txtUsuario.text.toString(), txtPassword.text.toString())
-
-    }
-    fun login(usuario:String, pass:String){
-
-        var btn1: Button = findViewById(R.id.btnLogin)
-        btn1.setOnClickListener {
-            Log.e("User", usuario)
-            Log.e("Password", pass)
-            if (usuario.equals("admin") && pass.equals("123")) {
-                var intent = Intent(this, Principal::class.java)
-                startActivity(intent)
-            } else {
-                Toast.makeText(
-                    applicationContext,
-                    "Error Credenciales Invalidas",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, Tabs::class.java))
         }
     }
 }
-//**********************************************************************************************
-//si corrio el app, le agregue el onclicklistener a el boton pero no jala el nombre y contrasena
-//**********************************************************************************************
